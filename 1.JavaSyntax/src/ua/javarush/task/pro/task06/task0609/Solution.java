474df0dfdb67b27803af2c967cbeedf17b174edd
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-01-26 16:55:30 +0100

    task7

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task06/task0609/Solution.java
@@ -0,0 +1,17 @@
+package ua.javarush.task.pro.task06.task0609;
+
+/* 
+Кубічний калькулятор у кубі
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long a){
+        return a*a*a;
+    }
+
+    //напишіть тут ваш код
+}
