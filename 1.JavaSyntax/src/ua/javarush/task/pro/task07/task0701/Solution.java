425e016ecadc70cccc4c47611601c8b4122787f6
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-01-27 16:53:20 +0100

    task 0701

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,18 @@
+package ua.javarush.task.pro.task07.task0701;
+
+/* 
+Усі можливі типи
+*/
+
+public class Solution {
+
+    byte a ;
+    short  b ;
+    int  c ;
+    long d ;
+    float f ;
+    double g ;
+    boolean h ;
+    char w ;
+
+}
