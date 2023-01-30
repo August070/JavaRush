7af5b4f431e328cac677515ef1704a245a2b07ac
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-01-30 15:48:48 +0100

    task 0709

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task07/task0709/Solution.java
@@ -0,0 +1,12 @@
+package ua.javarush.task.pro.task07.task0709;
+
+/* 
+Дуже правильні типи
+*/
+
+public class Solution {
+    long a = 109 + 15;
+    int b = (int) (a * 2);
+    short c = (short) (0);
+    byte d = (byte) (a + b - c);
+}
