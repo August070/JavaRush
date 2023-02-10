fb91f0b274cdbc01714a846619a6cbe5cca35974
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-10 21:07:07 +0100

    start task 1001

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1001/Skyscraper.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task10.task1001;
+
+/* 
+Створімо хмарочос
+*/
+
+public class Skyscraper {
+
+    //напишіть тут ваш код
+
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+}
