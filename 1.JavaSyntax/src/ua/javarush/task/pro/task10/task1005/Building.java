624b32e95a3fef6c57f635830f548c589a17f3b3
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-12 20:05:58 +0100

    start task 1005

--- 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1005/Building.java
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1005/Building.java
@@ -7,14 +7,12 @@ package ua.javarush.task.pro.task10.task1005;
 public class Building {
     private String type;
 
-    public Building(String type) {
+    public void initialize(String type) {
         this.type = type;
     }
 
-    //напишіть тут ваш код
-
     public static void main(String[] args) {
-        Building building = new Building("Ресторан");
+        Building building = new Building();
         building.initialize("Барбершоп");
     }
 }
