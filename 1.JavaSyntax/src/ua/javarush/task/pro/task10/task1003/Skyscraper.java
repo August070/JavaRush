2408d66cc50ef6e8dca17b17c1d578f16994f856
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-12 19:16:31 +0100

    start task 1003

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1003/Skyscraper.java
@@ -0,0 +1,17 @@
+package ua.javarush.task.pro.task10.task1003;
+
+/* 
+Збудуймо новий бізнес-комплекс JavaRush Business Center
+*/
+
+public class Skyscraper {
+    private int floorsCount;
+    private String developer;
+
+    //напишіть тут ваш код
+
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+        Skyscraper skyscraperUnknown = new Skyscraper(50, "Невідомо");
+    }
+}
