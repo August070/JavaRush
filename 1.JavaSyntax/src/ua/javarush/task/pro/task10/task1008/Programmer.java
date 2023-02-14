26aefd0f83abdfcbcb032a636b4abe9f8b2d997e
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-14 19:26:07 +0100

    done task 1008

--- 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1008/Programmer.java
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1008/Programmer.java
@@ -7,6 +7,13 @@ package ua.javarush.task.pro.task10.task1008;
 public class Programmer {
     private int salary = 1000;
 
-    //напишіть тут ваш код
+    public int getSalary() {
+        return salary;
+    }
 
+    public void setSalary(int salary) {
+        if (this.salary < salary) {
+            this.salary = salary;
+        }
+    }
 }
