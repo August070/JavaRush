b5b5a211a6ba51978143db63285b0e36d869c714
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-15 10:07:59 +0100

    start task 1015

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1015/Hedgehog.java
@@ -0,0 +1,19 @@
+package ua.javarush.task.pro.task10.task1015;
+
+/* 
+Їжачок і яблуко
+*/
+
+public class Hedgehog {
+
+    public void eat(Apple apple) {
+        System.out.println("Яблуко з'їли!");
+    }
+
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+
+    public static class Apple {
+    }
+}
