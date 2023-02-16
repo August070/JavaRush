b69291f723fe7b784b3bc9f9dc108a5cf3538019
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-16 09:57:38 +0100

    done task 1016

--- 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1016/Solution.java
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1016/Solution.java
@@ -4,13 +4,19 @@ package ua.javarush.task.pro.task10.task1016;
 Прогноз погоди
 */
 
+import java.io.Serializable;
+import java.util.Arrays;
+
 public class Solution {
 
+    private static City city;
+
     public static void showWeather(City city) {
-        //напишіть тут ваш код
+        System.out.println(String.format("У місті %s сьогодні температура повітря %d", city.getName(), city.getTemperature()));
     }
 
     public static void main(String[] args) {
-        //напишіть тут ваш код
+        City city = new City("Dubai", 40);
+        showWeather(city);
     }
 }
