c7877d6fd839de8b37bad83f27e5cb0eff1f21bb
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-16 09:40:23 +0100

    start task 1016

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1016/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.task.pro.task10.task1016;
+
+/* 
+Прогноз погоди
+*/
+
+public class Solution {
+
+    public static void showWeather(City city) {
+        //напишіть тут ваш код
+    }
+
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+}
