40fa9f57e5500e11a632d43388b0c4459ac6eedd
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-19 19:16:14 +0100

    done task 1017

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,17 @@
+package ua.javarush.task.pro.task10.task1017;
+
+/* 
+Створення материків
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        new Africa(1);
+        new Antarctica(2);
+        new Australia(3);
+        new Eurasia(4);
+        new NorthAmerica(5);
+new SouthAmerica(6);
+
+    }
+}
