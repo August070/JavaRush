e5975a1290ae0b2ddc29682941d4b0b2d7774b86
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-19 19:30:02 +0100

    start task 1021

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task10/task1021/Car.java
@@ -0,0 +1,14 @@
+package ua.javarush.task.pro.task10.task1021;
+
+/* 
+Автовиробник
+*/
+
+public class Car {
+    private String model;
+    private int year;
+    private String color;
+
+    //напишіть тут ваш код
+
+}
