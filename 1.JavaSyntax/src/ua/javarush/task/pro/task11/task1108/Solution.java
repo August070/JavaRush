71732260f90fbbff9da7f673bc80b78609d9cd99
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-20 18:58:26 +0100

    done task 1108

--- 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1108/Solution.java
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1108/Solution.java
@@ -4,31 +4,35 @@ package ua.javarush.task.pro.task11.task1108;
 Утилітний калькулятор
 */
 
+import java.util.concurrent.Callable;
+
 public class Solution {
     public static final String EQUAL = " = ";
 
     public static void main(String[] args) {
         int a = 45;
         int b = 15;
-        add(a, b);
-        subtract(a, b);
-        multiply(a, b);
-        divide(a, b);
+        Calculator.add(a,b);
+        Calculator.subtract(a, b);
+        Calculator.multiply(a, b);
+        Calculator.divide(a, b);
     }
 
-    public static void add(int a, int b) {
-        System.out.println(a + " + " + b + EQUAL + (a + b));
-    }
+    public static class Calculator {
+        public static void add(int a, int b) {
+            System.out.println(a + " + " + b + EQUAL + (a + b));
+        }
 
-    public static void subtract(int a, int b) {
-        System.out.println(a + " - " + b + EQUAL + (a - b));
-    }
+        public static void subtract(int a, int b) {
+            System.out.println(a + " - " + b + EQUAL + (a - b));
+        }
 
-    public static void multiply(int a, int b) {
-        System.out.println(a + " * " + b + EQUAL + (a * b));
-    }
+        public static void multiply(int a, int b) {
+            System.out.println(a + " * " + b + EQUAL + (a * b));
+        }
 
-    public static void divide(int a, int b) {
-        System.out.println(a + " / " + b + EQUAL + (a / b));
+        public static void divide(int a, int b) {
+            System.out.println(a + " / " + b + EQUAL + (a / b));
+        }
     }
 }
