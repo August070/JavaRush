38c49e251dfd40f7d09d52cdada4e0dad0bfc949
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-20 13:41:23 +0100

    done task 1104

--- 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1104/Solution.java
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1104/Solution.java
@@ -7,6 +7,7 @@ package ua.javarush.task.pro.task11.task1104;
 public class Solution {
     public static void main(String[] args) {
         String string = "12.84";
-        //напишіть тут ваш код
+        double temp = Double.parseDouble(string);
+        System.out.println((Math.round(temp)));
     }
 }
