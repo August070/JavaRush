06c7716d99900bfd38ac7a92d5b0c196fb4a1b67
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-20 18:59:48 +0100

    start task 1109

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1109/Solution.java
@@ -0,0 +1,11 @@
+package ua.javarush.task.pro.task11.task1109;
+
+/* 
+Об'єкти внутрішніх і вкладених класів
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишіть тут ваш код
+    }
+}
