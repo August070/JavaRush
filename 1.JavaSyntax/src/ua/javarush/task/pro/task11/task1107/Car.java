28095ae1aad00923f91b81f86ffda70d5c8848a2
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-20 14:08:21 +0100

    done task 1107

--- 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1107/Car.java
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1107/Car.java
@@ -5,6 +5,15 @@ package ua.javarush.task.pro.task11.task1107;
 */
 
 public class Car {
-    //напишіть тут ваш код
+    private Engine engine;
+    class Engine {
+        private boolean isRunning;
+        public void start(){
+            isRunning = true;
+        }
+        public void stop(){
+            isRunning = false;
+        }
+    }
 
 }
