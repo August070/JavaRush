186520a4fc975761edd191c86dd0915373497818
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-23 14:51:28 +0100

    done task 1120

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task11/task1120/Solution.java
@@ -0,0 +1,17 @@
+package ua.javarush.task.pro.task11.task1120;
+
+import javax.sound.sampled.Line;
+import java.util.Scanner;
+
+/* 
+Усі літери малі
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String line = console.nextLine();
+        line = line.toLowerCase();
+        System.out.println(line);
+    }
+}
