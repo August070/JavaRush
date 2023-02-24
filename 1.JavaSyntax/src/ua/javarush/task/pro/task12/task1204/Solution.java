0351608322b4c58cf9779135d50ae40fcc50fc80
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-24 13:07:24 +0100

    done task 1204

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task12/task1204/Solution.java
@@ -0,0 +1,21 @@
+package ua.javarush.task.pro.task12.task1204;
+
+/* 
+Сплата за рахунками
+*/
+
+public class Solution {
+    public static Integer balance;
+
+    public static void main(String[] args) {
+        String bill = "1234567890";
+
+        System.out.println("Поточний баланс : " + balance);
+        processPayment(bill);
+        System.out.println("Поточний баланс : " + balance);
+    }
+
+    public static void processPayment(String bill) {
+        //напишіть тут ваш код
+    }
+}
