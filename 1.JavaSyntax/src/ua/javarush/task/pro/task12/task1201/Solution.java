985f82247b2914a3bbe71407c0c14a735284796a
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-24 12:11:18 +0100

    start task 1201

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task12/task1201/Solution.java
@@ -0,0 +1,20 @@
+package ua.javarush.task.pro.task12.task1201;
+
+/* 
+Автоматичне пакування
+*/
+
+public class Solution {
+    byte byteValue;
+    short shortValue;
+    int intValue;
+    long longValue;
+
+    float floatValue;
+    double doubleValue;
+
+    char charValue;
+    boolean booleanValue;
+
+    //напишіть тут ваш код
+}
