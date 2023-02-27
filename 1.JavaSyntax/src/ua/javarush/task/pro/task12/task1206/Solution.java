3dcb5c2ea6a5b9aebbfa1108a1aa41a14ccb8855
Kostya Shkurovkiy <sk8august07@gmail.com>
2023-02-27 09:24:47 +0100

    start task 1206

---
+++ 1.JavaSyntax/src/ua/javarush/task/pro/task12/task1206/Solution.java
@@ -0,0 +1,28 @@
+package ua.javarush.task.pro.task12.task1206;
+
+/* 
+Аналіз рядків
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String string = "Гадаю, це буде нова фіча." +
+                "Тільки нікому не кажіть, що вона виникла випадково.";
+
+        System.out.println("Кількість цифр у рядку : " + countDigits(string));
+        System.out.println("Кількість літер у рядку : " + countLetters(string));
+        System.out.println("Кількість пробілів у рядку : " + countSpaces(string));
+    }
+
+    public static int countDigits(String string) {
+        //напишіть тут ваш код
+    }
+
+    public static int countLetters(String string) {
+        //напишіть тут ваш код
+    }
+
+    public static int countSpaces(String string) {
+        //напишіть тут ваш код
+    }
+}
